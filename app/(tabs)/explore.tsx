89ec310fcee47a6e35a413efd4db33c2144f9944
BenniSclano <benedetta@parkito.app>
2025-01-03 13:03:55 +0100

    installed storybook working

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -6,6 +6,7 @@ import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { IconSymbol } from '@/components/ui/IconSymbol';
+import { Button } from '~/components/ui/button';
 
 export default function TabTwoScreen() {
   return (
