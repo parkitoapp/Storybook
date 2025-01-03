fd22bd95b03775bbbf8b8313f62550f4fa0ed80d
BenniSclano <benedetta@parkito.app>
2025-01-03 17:18:14 +0100

    working expo

---
+++ app/(tabs)/TestComponents.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet, Image, Platform } from 'react-native';
+import { Collapsible } from '@/components/Collapsible';
+import { ExternalLink } from '@/components/ExternalLink';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+import { IconSymbol } from '@/components/ui/IconSymbol';
+import { Button } from '@/components/ui/button';
+
+
