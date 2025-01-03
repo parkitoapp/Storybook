4baee1a48773ebf2641338a3e4adb1bb97e6e195
BenniSclano <benedetta@parkito.app>
2025-01-03 10:44:42 +0100

    start restyle project

--- .storybook/stories/test/test.js
+++
@@ -1,7 +0,0 @@
-export default function Test(props){
-    return <h1 style={{
-        backgroundColor: props.backgroundColor,
-        color: props.color,
-        padding: props.padding
-    }}>Test</h1>
-}
--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    content: [],
-    content: ["./App.{js,jsx,ts,tsx}", "./<custom directory>/**/*.{js,jsx,ts,tsx}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
